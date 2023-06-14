d19d8f33165cc21a34b1fa7f9d562299e9f30b17
khakimdon <almamedovkh@mail.ru>
2023-06-14 17:36:47 +0500

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { createStore } from "vuex";
+
+export default createStore({
+  state: {
+    arr: 'asdasd'
+  },
+  getters: {
+    arr: state => state.arr
+  },
+  mutations: {
+
+  },
+  actions: {
+   
+  },
+  modules: {},
+});
