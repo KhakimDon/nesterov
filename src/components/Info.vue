930ca7b833fafae595ff86b9bfdc6df79482e75d
khan <110159689+Mckhan1@users.noreply.github.com>
2023-06-16 00:39:31 +0500

    sec comm

---
+++ src/components/Info.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+
+
+<template>
+
+<div class="mt-[500px]">
+    <h1>vayy diiiina ibi</h1>
+</div>
+
+</template>
+
+
+
+<style></style>
---
+++ src/views/InfoView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Info from '../components/Info.vue';
+
+</script>
+
+<script>
+export default {
+    components: { Info }
+}
+</script>
+
+<template>
+    <div>
+        <Info />
+    </div>
+</template>
+
+
+
+<style></style>
